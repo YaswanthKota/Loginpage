3709046b563e58c4964eb53a50da5abbf8ecced5
yaswanthkota <yaswanthsivaram.kota@gmail.com>
2020-10-07 11:32:27 +0530

    Updates

---
+++ precache-manifest.d9231560940e6f2d18b5759295214330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1a3666d915412a04f84458b09b229a",
+    "url": "/Loginpage/index.html"
+  },
+  {
+    "revision": "734896f519d22ec4cc6f",
+    "url": "/Loginpage/static/css/main.2ef24645.chunk.css"
+  },
+  {
+    "revision": "ff600fca81b9ae54852d",
+    "url": "/Loginpage/static/js/2.78f766bf.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Loginpage/static/js/2.78f766bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734896f519d22ec4cc6f",
+    "url": "/Loginpage/static/js/main.39cb981f.chunk.js"
+  },
+  {
+    "revision": "6603558cfe921e64da40",
+    "url": "/Loginpage/static/js/runtime-main.5f5fa5c6.js"
+  }
+]);
